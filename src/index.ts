2fe78e88e2dc69be6ef19bd7aeb1a89d04b81721
khuongduy354 <khuongduy354@gmail.com>
2022-07-10 14:51:54 +0700

    basic centralizer error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //boostrap expressjs
-import express from "express";
+import express, { Request, Response } from "express";
 import "dotenv/config";
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,4 +12,19 @@ app.get("/helloworld", (req, res) => {
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT.toString());
+});
+
+// Error handling
+app.use((err: Error, req: Request, res: Response) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Error" });
+});
+
+process.on("uncaughtException", (e) => {
+  console.log(e);
+  process.exit(1);
+});
+process.on("unhandledRejection", (e) => {
+  console.log(e);
+  process.exit(1);
 });
