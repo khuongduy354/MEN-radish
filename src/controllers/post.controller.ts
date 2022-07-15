8964ae2a14d242f7a3bfe2764e24cc20409f42f5
khuongduy354 <khuongduy354@gmail.com>
2022-07-15 16:31:41 +0700

    feat(post):basic routes done
    pagination, ranking not implement yet

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,53 +1,33 @@
-import { Subreddit } from "./../models/subreddit.model";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.model";
 import { Post } from "../models/post.model";
+import { PostService } from "../service/post.service";
 
-const createPost = async (req: Request, res: Response) => {
+const createPost = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    //prepare
+    const username = req.username;
     const { title, subredditName, textSubmission } = req.body;
-
-    const author = await User.findOne({ username: req.username });
-    const subreddit = await Subreddit.findOne({
-      subredditName: subredditName,
-    });
-    if (!author) {
-      return res
-        .status(404)
-        .send({ message: "User does not exist in database." });
-    }
-
-    if (!subreddit) {
-      return res.status(404).send({
-        message: `Subreddit with ID: '${subreddit}' does not exist in database.`,
-      });
-    }
-
-    const post = await Post.create({
+    //pass to service layer
+    const post = await PostService.createPost({
       title,
-      author: author._id,
-      subreddit: subreddit.subredditName,
+      subredditName,
       textSubmission,
+      username,
     });
-    author.posts = author.posts.concat(post._id);
-    await author.save();
-    subreddit.posts = subreddit.posts.concat(post._id);
-    await subreddit.save();
-    await post
-      .populate("author", "username")
-      .populate("subreddit", "subredditName")
-      .execPopulate();
 
     res.status(200).json({ data: post });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 const getPost = async (req: Request, res: Response) => {
   try {
+    //prepare data
     const { id } = req.params;
-    const post = await Post.findById(id)
-      .populate("author", "username")
-      .populate("subreddit", "subredditName")
-      .populate("comments");
+    //pass to service
+    const post = await PostService.getPost(Number(id));
+
     res.status(200).json({ data: post });
   } catch (e) {}
 };
@@ -68,120 +48,83 @@ const getPostsOnSearch = async (req: Request, res: Response) => {
   } catch (e) {}
 };
 
-const updatePost = async (req: Request, res: Response) => {
+const updatePost = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    //prepare
     const { id } = req.params;
     const { textSubmission } = req.body;
-    //update post
-    const post = await Post.findByIdAndUpdate(id, textSubmission, {
-      new: true,
+    //pass to service
+    const post = await PostService.updatePost({
+      id: Number(id),
+      textSubmission,
     });
-    if (!post) return res.status(404).send({ message: "Post does not exist." });
-    //check if user is author
-    if (post.author !== req.username)
-      return res.status(401).send({ message: "Unauthorized" });
-    //populate
-    post
-      .populate("author", "username")
-      .populate("subreddit", "subredditName")
-      .execPopulate();
     res.status(200).json({ data: post });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 const deletePost = async (req: Request, res: Response) => {
   try {
+    //prepare
     const { id } = req.params;
+    const username = req.username;
+    //pass to service
+    const post = await PostService.deletePost(Number(id), username);
 
-    //check if user is author
-    const post = await Post.findById(id).populate("author", "username");
-    const subreddit = await Subreddit.findById(post.subredditName);
-    const author = await User.findById(req.username);
-    if (post.author.username !== req.username)
-      return res.status(401).send({ message: "Unauthorized" });
-
-    //delete post
-    await Post.findByIdAndDelete(id);
-
-    //delete post in subreddit
-    subreddit.posts = subreddit.posts.filter(
-      (subPostId: any) => subPostId !== post._id
-    );
-    await subreddit.save();
-
-    //delete post in author
-    author.posts = post.author.posts.filter(
-      (userPostId: any) => userPostId !== post._id
-    );
-    await author.save();
     res.status(200).json({ data: post });
   } catch (e) {}
 };
-const getSubscribedPosts = async (req: Request, res: Response) => {
+const getSubscribedPosts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
+    //prepare data
     const username = req.username;
-    const user = await User.findOne({ username }).populate("subscribedSubs");
-    const posts = await Post.find({
-      _id: { $in: user.subscribedSubs },
-    })
-      .select("-comments")
-      .populate("author", "username")
-      .populate("subreddit", "subredditName");
+
+    //pass to service
+    const posts = await PostService.getSubscribedPosts(username);
 
     res.status(200).json({ data: posts });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 
-const upvotePost = async (req: Request, res: Response) => {
+const upvotePost = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    const user = await User.findOne({ username: req.username });
-    const post = await Post.findById(id);
-    //if user has already upvoted, remove upvote
-    if (post.upvotedBy.includes(user._id)) {
-      post.upvotedBy = post.upvotedBy.filter(
-        (userId: any) => userId !== user._id
-      );
-      await post.save();
-    } else {
-      post.upvotedBy.push(user._id);
-      //remove downvote if upvote
-      post.downvotedBy = post.downvotedBy.filter(
-        (userId: any) => userId !== user._id
-      );
-      await post.save();
-    }
+    const username = req.username;
+    //pass to service
+    const post = await PostService.upvotePost(Number(id), username);
     res.status(200).json({ data: post });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 
-const downvotePost = async (req: Request, res: Response) => {
+const downvotePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { id } = req.params;
-    const user = await User.findOne({ username: req.username });
-    const post = await Post.findById(id);
-    //if user has already downvoted, remove downvote
-    if (post.downvotedBy.includes(user._id)) {
-      post.downvotedBy = post.downvotedBy.filter(
-        (userId: any) => userId !== user._id
-      );
-      await post.save();
-    } else {
-      post.downvotedBy.push(user._id);
-      //remove upvote if downvote
-      post.upvotedBy = post.upvotedBy.filter(
-        (userId: any) => userId !== user._id
-      );
-      await post.save();
-    }
+    const username = req.username;
+    //pass to service
+    const post = await PostService.downvotePost(Number(id), username);
     res.status(200).json({ data: post });
-  } catch (e) {}
+  } catch (e) {
+    next(e);
+  }
 };
 
 export default {
-  createPost, //☑️
-  updatePost, //☑️
   getPostsOnSearch,
   getAllPosts,
+  createPost, //☑️
+  updatePost, //☑️
   deletePost, //☑️
   getPost, // ☑️
   getSubscribedPosts, //☑️
--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -1,27 +1,198 @@
+import { AppError } from "./../error";
 import { Post } from "../models/post.model";
+import { Subreddit } from "../models/subreddit.model";
+import { User } from "../models/user.model";
 
-const createProduct = async () => {
-  try {
-  } catch (e) {}
+type createPostProp = {
+  title: string;
+  subredditName: string;
+  textSubmission: string;
+  username: string;
 };
-const getProduct = async () => {
+const createPost = async (createPostProp: createPostProp) => {
   try {
-  } catch (e) {}
+    //prepare
+    const { username, title, subredditName, textSubmission } = createPostProp;
+    const author = await User.findOne({ username });
+    const subreddit = await Subreddit.findOne({
+      subredditName: subredditName,
+    });
+
+    //ensure exists
+    if (!author) {
+      throw new AppError(404, "Author does not exist");
+    }
+
+    if (!subreddit) {
+      throw new AppError(404, "Subreddit does not exist");
+    }
+
+    //create post
+    const post = await Post.create({
+      title,
+      author: author._id,
+      subreddit: subreddit.subredditName,
+      textSubmission,
+    });
+    author.posts = author.posts.concat(post._id);
+    await author.save();
+    subreddit.posts = subreddit.posts.concat(post._id);
+    await subreddit.save();
+    await post.populate("author", "username");
+    await post.populate("subreddit", "subredditName");
+    return post;
+  } catch (e) {
+    throw e;
+  }
+};
+const getPost = async (id: number) => {
+  try {
+    const post = await Post.findById(id)
+      .populate("author", "username")
+      .populate("subreddit", "subredditName")
+      .populate("comments");
+    return post;
+  } catch (e) {
+    throw e;
+  }
 };
 
-const updateProduct = async () => {
-  try {
-    Post.findOne();
-  } catch (e) {}
+type updatePostProp = {
+  id: number;
+  textSubmission: string;
 };
-const deleteProduct = async () => {
+const updatePost = async (updatePostProp: updatePostProp) => {
   try {
-  } catch (e) {}
+    const { textSubmission, id } = updatePostProp;
+    //update post
+    const post = await Post.findByIdAndUpdate(
+      id,
+      { textSubmission },
+      {
+        new: true,
+      }
+    );
+
+    //ensure exists
+    if (!post) {
+      throw new AppError(404, "Post does not exist");
+    }
+
+    await post.populate("author", "username");
+    await post.populate("subreddit", "subredditName");
+    return post;
+  } catch (e) {
+    throw e;
+  }
+};
+const deletePost = async (id: number, username: string) => {
+  try {
+    //prepare and ensure exists
+    const post = await Post.findById(id);
+    const author = await User.findById(username);
+
+    if (!post) throw new AppError(404, "Post does not exist");
+    if (!author) throw new AppError(404, "Author does not exist");
+
+    const subreddit = await Subreddit.findById(post.subreddit);
+    if (!subreddit) throw new AppError(404, "Subreddit does not exist");
+
+    //delete posts
+    await Post.findByIdAndDelete(id);
+
+    //remove post from subreddit
+    const postId = post._id;
+    subreddit.posts = subreddit.posts.filter((_postId) => _postId !== postId);
+    await subreddit.save();
+
+    //remove post from author
+    author.posts = author.posts.filter((_postId) => _postId !== postId);
+    await author.save();
+
+    return post;
+  } catch (e) {
+    throw e;
+  }
+};
+const upvotePost = async (id: number, username: string) => {
+  try {
+    const user = await User.findOne({ username });
+    const post = await Post.findById(id);
+    //ensure exists
+    if (!user) throw new AppError(404, "User does not exist");
+    if (!post) throw new AppError(404, "Post does not exist");
+
+    //toggle upvote
+    if (post.upvotedBy.includes(user._id)) {
+      post.upvotedBy = post.upvotedBy.filter((userId) => userId !== user._id);
+    } else {
+      post.upvotedBy.push(user._id);
+    }
+
+    //remove downvote if exists
+    post.downvotedBy = post.downvotedBy.filter(
+      (userId: any) => userId !== user._id
+    );
+    await post.save();
+    return post;
+  } catch (e) {
+    throw e;
+  }
+};
+const downvotePost = async (id: number, username: string) => {
+  try {
+    const user = await User.findOne({ username });
+    const post = await Post.findById(id);
+    //ensure exists
+    if (!user) throw new AppError(404, "User does not exist");
+    if (!post) throw new AppError(404, "Post does not exist");
+
+    //toggle downvote
+    if (post.downvotedBy.includes(user._id)) {
+      post.downvotedBy = post.downvotedBy.filter(
+        (userId) => userId !== user._id
+      );
+    } else {
+      post.downvotedBy.push(user._id);
+    }
+
+    //remove upvote if exists
+    post.upvotedBy = post.upvotedBy.filter(
+      (userId: any) => userId !== user._id
+    );
+    await post.save();
+    return post;
+  } catch (e) {
+    throw e;
+  }
+};
+const getSubscribedPosts = async (username: string) => {
+  try {
+    //prepare data
+    const user = await User.findOne({ username }).populate("subscribedSubs");
+    if (!user) throw new AppError(404, "User does not exist");
+
+    const posts = await Post.find({
+      _id: { $in: user.subscribedSubs },
+    })
+      .select("-comments")
+      .populate("author", "username")
+      .populate("subreddit", "subredditName");
+
+    if (!posts) throw new AppError(404, "Post does not exist");
+
+    return posts;
+  } catch (e) {
+    throw e;
+  }
 };
 
-export default {
-  createProduct,
-  updateProduct,
-  getProduct,
-  deleteProduct,
+export const PostService = {
+  createPost,
+  updatePost,
+  getPost,
+  deletePost,
+  getSubscribedPosts,
+  upvotePost,
+  downvotePost,
 };
