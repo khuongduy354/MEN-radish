fe35a1c1df5a78a7dfda4f8fa4d4b13a2f17b00c
khuongduy354 <khuongduy354@gmail.com>
2022-07-17 11:37:56 +0700

    feat(auth):validate user in middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,31 @@
+import { AppError } from "./../error";
 import { NextFunction, Request, Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import { parseJWT } from "../helper/jwtTool";
+import { User } from "../models/user.model";
 
-export const auth = (req: Request, res: Response, next: NextFunction) => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    //get token
     const token =
       req.headers["x-auth-token"] ||
       req.body.token ||
       req.headers["authorization"];
-    if (!token) return res.status(400).json({ message: "No token provided" });
+
+    if (!token) next(new AppError(401, "Invalid token"));
+
+    //get username from token
     const parsedToken = parseJWT(token);
-    req.username = (parsedToken as JwtPayload).username;
-    next();
+    const username = (parsedToken as JwtPayload).username;
+
+    //ensures user exists
+    if (await User.exists({ username })) {
+      req.username = username;
+      next();
+    } else {
+      next(new AppError(401, "Invalid token"));
+    }
   } catch (e) {
-    res.status(401).json({ message: "Invalid token" });
+    next(new AppError(401, "Invalid token"));
   }
 };
